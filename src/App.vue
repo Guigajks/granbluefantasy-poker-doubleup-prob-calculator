875ae5b5e6c7a12cd366840eb3699c7a69bbc19d
guigajks <guigajks@gmail.com>
2021-06-14 13:53:10 -0300

    feat: card images

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,56 @@
 <template>
-  <div>
-    oi
+  <div id="test">
+    <!-- <template v-for="suit in suits"> -->
+    <!-- <img
+        v-for="number in numbers"
+        :key="`${number}${suit}`"
+        :src="`${require(`@/assets/cards/${number}${suit}.jpg`)}`"
+        width="75"
+        height="100"
+        loading="lazy"
+      /> -->
+    <!-- </template> -->
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+
+  data() {
+    return {
+      suits: ['H', 'D', 'S', 'C'],
+      numbers: [
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '10',
+        'J',
+        'Q',
+        'K',
+        'A',
+      ],
+      cards: {},
+      cardsDir: './assets/cards',
+    };
+  },
+
+  async created() {
+    for (const number of this.numbers) {
+      for (const suit of this.suits) {
+        this.cards[`${number}${suit}`] = false;
+      }
+    }
+
+    console.log(this.cards);
+  },
+
+  methods: {},
 };
 </script>
 
